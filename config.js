e795cb8198ec0f7feaafb00d21a9dfa5d92573f2
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2026-08-26 19:29:55 -0300

    Add configuration file for server settings

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // IDs do servidor
+    guildId: "ID_DO_SEU_SERVIDOR",
+    
+    // Cargos
+    cargoAtendente: "ID_CARGO_ATENDENTE",
+    cargoMembro: "ID_CARGO_MEMBRO",
+    cargoVeterano: "ID_CARGO_VETERANO",
+    cargoADM: "ID_CARGO_ADM",
+    cargoDono: "ID_CARGO_DONO",
+    
+    // Categorias
+    categoriaIntro: "ID_CATEGORIA_INTRO",
+    categoriaAtendimento: "ID_CATEGORIA_ATENDIMENTO",
+    categoriaSobre: "ID_CATEGORIA_SOBRE",
+    
+    // Canais
+    canalBoasVindas: "ID_CANAL_BOAS_VINDAS",
+    canalInstrucoes: "ID_CANAL_INSTRUCOES",
+    canalVenda: "ID_CANAL_VENDA_SEUS_GMAILS",
+    canalExplicacoes: "ID_CANAL_EXPLICACOES",
+    canalAtendimento: "ID_CANAL_ATENDIMENTO",
+    
+    prefix: "."
+};
